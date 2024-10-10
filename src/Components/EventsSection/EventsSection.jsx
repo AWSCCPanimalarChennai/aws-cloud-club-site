2ebc102685790404aa18ceab05a575bb69784853
Rishab59 <hrishab59@gmail.com>
2024-10-10 20:35:29 +0530

    Template created for Events page

--- src/Components/EventsSection/EventsSection.jsx
+++ src/Components/EventsSection/EventsSection.jsx
@@ -7,13 +7,14 @@ import SectionHeader from "../SectionHeader/SectionHeader";
 import EventsCard from './EventsCard/EventsCard';
 import DetailsCard from './DetailsCard/DetailsCard';
 
-import inaugurationPoster from '../../assets/img/posters/inauguration.jpg';
-import meetup2 from '../../assets/img/posters/meetup2.jpg';
-import meetup3 from '../../assets/img/posters/meetup3.jpg';
-import meetup4 from '../../assets/img/posters/meetup4.jpeg';
-import meetup5 from '../../assets/img/posters/meetup5.jpg';
-import meetup6 from '../../assets/img/posters/meetup6.jpg';
+// import inaugurationPoster from '../../assets/img/posters/inauguration.jpg';
+// import meetup2 from '../../assets/img/posters/meetup2.jpg';
+// import meetup3 from '../../assets/img/posters/meetup3.jpg';
+// import meetup4 from '../../assets/img/posters/meetup4.jpeg';
+// import meetup5 from '../../assets/img/posters/meetup5.jpg';
+// import meetup6 from '../../assets/img/posters/meetup6.jpg';
 import noEvents from '../../assets/animation/noEvents.json'
+import { eventsSectionRowData } from '../../data/eventsSectionRowData';
 
 function EventsSection() {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -43,7 +44,65 @@ function EventsSection() {
 
       <SectionHeader header="Past Events" />
 
-        <Row>
+      <span style = {{ marginTop: "40px" }}></span>
+
+      {
+        eventsSectionRowData.map((ele, index) => {
+          return (
+            <>
+              <Row key = { index }>
+                <Col lg = { ele.col1lg }>
+                  <EventsCard
+                    image = { ele.eventCardImage }
+                    title = { ele.eventCardTitle }
+                  />
+                </Col>
+
+                <Col 
+                  lg = { ele.col2lg }
+                  className = "event-desc"
+                >
+                  {
+                    ele.h1 && (
+                      <h1 className = "event-title">
+                        { ele.h1 }
+                      </h1>
+                    )
+                  }
+
+                  {
+                    ele.p && (
+                      <p
+                        className = "para"
+                        style = {{
+                          marginBottom: "40px"
+                        }}
+                      >
+                        { ele.p }
+                      </p>
+                    )
+                  }
+
+                  <DetailsCard
+                    speaker = { ele.speaker }
+                    mode = { ele.mode } 
+                    date = { ele.date } 
+                    time = { ele.time } 
+                    participants = { ele.participants }
+                    sponsor = { ele.sponsor }
+                    eventLink = { ele.eventLink }
+                  />
+                </Col>
+              </Row>
+
+              <br />
+              <br />
+            </>
+          );
+        })
+      }
+
+        {/* <Row>
             <Col lg={7}>
             <EventsCard style={{marginTop: "40px"}} image={meetup6} title="Meetup-6 (Oct 2024)"></EventsCard>
             </Col>
@@ -189,7 +248,7 @@ function EventsSection() {
               participants="241" sponsor="--"
               eventLink="https://www.meetup.com/aws-cloud-club-panimalar-chennai/events/300159208/"></DetailsCard>
             </Col>
-          </Row>
+          </Row> */}
     </Container>
   );
 }
